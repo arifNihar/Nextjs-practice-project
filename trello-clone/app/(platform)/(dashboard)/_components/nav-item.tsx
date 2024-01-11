a1e7c6868e3c2512da24faa9b4bdb4a8ad5fabcd
arifNihar <arif.khan.nihar.90@gmail.com>
2024-01-11 23:59:34 +0600

    new featur

---
+++ trello-clone/app/(platform)/(dashboard)/_components/nav-item.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+
+import {useRouter, usePathname} from "next/navigation";
+import Image from "next/image";
+import {
+    Activity,
+    CreditCard,
+    Layout,
+    Settings,
+} from "lucide-react";
+
+import { cn } from "@/lib/utils";
+
+import {
+    AccordionContent,
+    AccordionItem,
+    Acc
+}
