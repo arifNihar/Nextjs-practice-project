2b71f2fea2e093f24353a13fcfe39c66a261c3c4
arifNihar <arif.khan.nihar.90@gmail.com>
2024-01-16 23:49:46 +0600

    trello new commit

--- trello-clone/app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ trello-clone/app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,7 +1,19 @@
+import { create } from "@/actions/create-board";
+import { Button } from "@/components/ui/button";
+
 const OrganizationIdPage = () => {
   return (
     <div>
-      <h2>Organization Id Page</h2>
+      <form action={create} className="flex gap-2">
+        <input
+          id="title"
+          name="title"
+          required
+          placeholder="Enter a board title"
+          className="border-sky-500 border p-1 rounded-sm"
+        />
+        <Button type="submit">Submit</Button>
+      </form>
     </div>
   );
 };
